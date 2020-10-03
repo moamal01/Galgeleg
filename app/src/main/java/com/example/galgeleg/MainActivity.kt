b68127e3b5a0018f176ce6732d91b9c481dd38cd
Momo <s195398@student.dtu.dk>
2020-10-03 21:38:19 +0200

    the game now shows the hangman

--- app/src/main/java/com/example/galgeleg/MainActivity.kt
+++ app/src/main/java/com/example/galgeleg/MainActivity.kt
@@ -2,11 +2,36 @@ package com.example.galgeleg
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    val galgelogik: Galgelogik = Galgelogik()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Sæt onClickListener
+        gæt_button.setOnClickListener(this)
+
+        galgelogik.startNytSpil()
+    }
+
+    override fun onClick(view: View?) {
+        val bogstav: String = letter_guess.text.toString().toLowerCase(Locale.ROOT)
+
+        galgelogik.gætBogstav(bogstav)
+        gættede_bogstav_textView.text = galgelogik.brugteBogstaver.toString()
+
+        when(galgelogik.antalForkerteBogstaver) {
+            1 -> hangman_imageView.setImageResource(R.drawable.forkert1)
+            2 -> hangman_imageView.setImageResource(R.drawable.forkert2)
+            3 -> hangman_imageView.setImageResource(R.drawable.forkert3)
+            4 -> hangman_imageView.setImageResource(R.drawable.forkert4)
+            5 -> hangman_imageView.setImageResource(R.drawable.forkert5)
+            6 -> hangman_imageView.setImageResource(R.drawable.forkert6)
+        }
     }
 }
